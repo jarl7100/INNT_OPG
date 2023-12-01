ecef036bd6d27bba342b71a5686e0c238a4bca97
jarl7100 <jarl7100@gmail.com>
2023-12-01 10:31:45 +0100

    Har givet iconer til tabnavigator for boatowner

--- Components/Navigation/TabNavigatorBoatOwner.js
+++ Components/Navigation/TabNavigatorBoatOwner.js
@@ -11,11 +11,44 @@ const Tab = createBottomTabNavigator();
 
 export default function TabNavigatorBoatOwner() {
     return (
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreenBoatOwner} />
+      <Tab.Navigator  screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "blue",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: [
+          {
+            display: "flex"
+          },
+          null
+        ],
+        // Her laver vi ikonerne til de forskellige sider i appen
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === 'Home') {
+            return (<Ionicons
+              name="home-outline"
+              size={size}
+              color={color}
+            />
+            );
+          }
+          else if (route.name === 'AddBoat') {
+            return (
+              <Ionicons
+                name="add-outline"
+                size={size}
+                color={color}
+              />
+            );
+          }
+        },
+      })}
+      // Her laver vi de forskellige sider i appen
+      >
+
+        <Tab.Screen name="Home"  component={HomeScreenBoatOwner}/>
         <Tab.Screen name="AddBoat" component={AddBoat} />
       </Tab.Navigator>
     )
   }
+
   
   
