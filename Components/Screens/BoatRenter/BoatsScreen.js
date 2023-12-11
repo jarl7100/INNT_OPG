a014dd036a49dabe1664d7ade18556693b0860e3
Lucas Ingemann Hunt <lucashunt1@gmail.com>
2023-12-11 21:32:07 +0100

    boat

--- Components/Screens/BoatRenter/BoatsScreen.js
+++ Components/Screens/BoatRenter/BoatsScreen.js
@@ -1,59 +1,124 @@
-import * as React from "react";
-import { Text, FlatList, Alert, SafeAreaView, View, StyleSheet } from "react-native";
-import { Button, Card } from 'react-native-paper';
-import { useState, useEffect } from 'react';
-import { useNavigation } from '@react-navigation/native';
-import Pocketbase from 'pocketbase';
-import Style from '../../GlobalStyleSheet/Style.js';
-
-
-function BoatsScreen() {
-    const navigation = useNavigation();
-    const fetchBoats = async () => {
-
-        const pb = new Pocketbase('https://pocketbaselucashunt.fly.dev');
-        const data = await pb.collection('boatPosts').getList(1, 10, {
-            sort: 'created',
-        });
-        setBoats(data.items);
-
-    }
-
+import {
+    Text,
+    FlatList,
+    Alert,
+    SafeAreaView,
+    View,
+    StyleSheet,
+  } from "react-native";
+  import { Button, Card } from "react-native-paper";
+  import { useState, useEffect } from "react";
+  import { useNavigation } from "@react-navigation/native";
+  import Pocketbase from "pocketbase";
+  import Style from "../../GlobalStyleSheet/Style.js"
+  import LoadingScreen from "../LoadingScreen.js";
+  
+  
+  const BoatsScreen = ({ route }) => {
+    const { filter, dateStart, dateEnd } = route.params || { filter: "", dateStart: "", dateEnd: "" };
+    const [searching, setSearching] = useState(true);
+    const [errorSearching, setErrorSearching] = useState("");
     const [boats, setBoats] = useState([]);
-    useEffect(() => {
-        fetchBoats();
-    }, [])
+    const navigation = useNavigation();
     
-
-    let BoatCards = ({ item }) => {
-        return (
-
-            <Card style={Style.boatCard} onPress={() => navigation.navigate("Boat Post Renter", { boatID: item.id })}>
-
+    useEffect(() => {
+      
+      fetchBoats();
+    }, [route]);
+  
+    const fetchBoats = async () => {
+    setBoats([]);
+    setErrorSearching("");
+      const pb = new Pocketbase("https://pocketbaselucashunt.fly.dev");
+      try {
+        if (filter !== "") {
+       
+          const resultList = await pb.collection("boatPosts").getList(1, 10, {
+            sort: "-created",
+            filter: filter,
+            expand: "starred"
+          });
         
-                <Card.Title  title={item.boatTitle}/>
-                <Card.Cover style={Style.boatCardImage} source={{ uri: "https://scdn.malibuboats.dev/cdn.pursuitboats.com/images/HomeNews/WOUNDER-70.webp" }} />
-                <Card.Content>
-                    <Text style={Style.boatCardText}>Pris: {item.boatPrice} kr./Dag</Text>
-
-            
-                </Card.Content>
-            </Card>
-        )
+          const startDate = new Date(dateStart);
+            const endDate = new Date(dateEnd);
+  
+          const filteredPosts = resultList.items.filter((post) => {
+            const resultDateStart = new Date(post.dateStart);
+            const resultDateEnd = new Date(post.dateEnd);
+    
+            return startDate >= resultDateStart && endDate <= resultDateEnd;
+          });
+          console.log(filteredPosts);
+          if (filteredPosts.length === 0) {
+            setErrorSearching("Der er ingen både der matcher din søgning!");
+        
+            } else {
+                setBoats(filteredPosts);
+            }
+          setSearching(false);
+        } else {
+            const resultList = await pb.collection("boatPosts").getList(1, 10, {
+                sort: "-created",
+            })
+            setBoats(resultList.items);
+            setSearching(false);
+        }
+        } catch (error) {
+          console.error("Error:", error);
+          setSearching(false);
+        }
     };
+  
+    let BoatCards = ({ item }) => {
+      return (
+        <Card
+          style={Style.boatCard}
+          onPress={() =>
+            navigation.navigate("Boat Post Renter", { boatID: item.id })
+          }
+        >
+          <Card.Title title={item.boatTitle} />
+          <Card.Cover
+            style={Style.boatCardImage}
+            source={{
+              uri: "https://scdn.malibuboats.dev/cdn.pursuitboats.com/images/HomeNews/WOUNDER-70.webp",
+            }}
+          />
+          <Card.Content>
+            <Text style={Style.boatCardText}>Pris: {item.boatPrice} kr./Dag</Text>
+          </Card.Content>
+        </Card>
+      );
+    };
+  
     return (
-        <View>
-        <SafeAreaView contentContainerStyle={Style.container}>
-            {/* Her laver vi en flatlist som viser alle bådene i databasen */}
-            <FlatList
-            style={Style.flatListCards}
-                data={boats}
-                renderItem={BoatCards}
-                keyExtractor={(item) => item.id}
-            />
-        </SafeAreaView>
-        </View>
+      <View style={Style.container}>
+          {searching ? <LoadingScreen /> : (
+              <>
+        <Text style={{ fontSize: 24, fontWeight: "bold" }}>{errorSearching}</Text>
+        <FlatList
+          style={Style.flatListCards}
+          data={boats}
+          renderItem={BoatCards}
+          keyExtractor={(item) => item.id}
+        />
+        </>
+        )}
+      </View>
     );
-}
-
-export default BoatsScreen
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: "bold",
+    },
+  });
+  
+  export default BoatsScreen;
+  
